4081d5cb20e60cd65aa161a7f78a36b36a9946ca
Sandy Solaqa <ssolaqa8226@sdsu.edu>
2023-06-06 23:02:34 -0700

    clean data

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read the Excel file
+# new_data = pd.read_excel("12hour_data.xlsx")
+new_data = pd.read_excel("15minute_data_california.xlsx")
+# Drop columns with missing values
+new_data.dropna(axis=1, inplace=True)
+
+# Save the modified data to a new Excel file
+new_data.to_excel('new_file.xlsx', index=False)
